506c34c7514895985d587e2037a35178e8d6f7e9
oguzhandogdu <oguzd494@gmail.com>
2024-01-12 21:52:23 +0300

    Add top bar to favorites screen

--- app/src/main/java/com/oguzdogdu/walliescompose/features/favorites/FavoritesScreen.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/features/favorites/FavoritesScreen.kt
@@ -4,15 +4,18 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.expandHorizontally
 import androidx.compose.animation.shrinkHorizontally
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -22,6 +25,8 @@ import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -32,6 +37,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -63,34 +69,50 @@ fun FavoritesScreenRoute(
         viewModel.handleUIEvent(FavoriteScreenEvent.GetFavorites)
 
     }
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        if (state.loading) {
-            LoadingState()
+    Scaffold(modifier = modifier
+        .fillMaxSize()
+        .background(Color.Magenta), topBar = {
+        Row(
+            modifier = modifier
+                .wrapContentWidth()
+                .padding(start = 16.dp, top = 16.dp, bottom = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(id = R.string.favorites_title),
+                style = MaterialTheme.typography.titleMedium,
+            )
         }
-        if (state.favorites?.isNotEmpty() == true) {
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(2),
-                modifier = modifier
-                    .fillMaxSize()
-                    .padding(horizontal = 8.dp),
-                state = rememberLazyGridState(),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                items(state.favorites.orEmpty()) {
-                    FavoritesImageView(imageUrl = it.url)
+    }) {
+        Column(
+            modifier = modifier.padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            if (state.loading) {
+                LoadingState()
+            }
+            if (state.favorites?.isNotEmpty() == true) {
+                LazyVerticalGrid(
+                    columns = GridCells.Fixed(2),
+                    modifier = modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 8.dp),
+                    state = rememberLazyGridState(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp)
+                ) {
+                    items(state.favorites.orEmpty()) {
+                        FavoritesImageView(imageUrl = it.url)
+                    }
                 }
             }
-        }
-        if (state.favorites.isNullOrEmpty()) {
-            EmptyView(modifier = modifier, state = state)
+            if (state.favorites.isNullOrEmpty()) {
+                EmptyView(modifier = modifier, state = state)
+            }
         }
     }
-
 }
 
 
