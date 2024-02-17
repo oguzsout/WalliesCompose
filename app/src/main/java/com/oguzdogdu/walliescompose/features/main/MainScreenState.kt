b762d0eea3f80edd3121cdebfb09e07a263b6b98
oguzhandogdu <oguzd494@gmail.com>
2024-02-17 15:52:02 +0300

    Edit state management of sign in method

--- app/src/main/java/com/oguzdogdu/walliescompose/features/main/MainScreenState.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/features/main/MainScreenState.kt
@@ -1,3 +1,6 @@
 package com.oguzdogdu.walliescompose.features.main
 
+import androidx.compose.runtime.Stable
+
+@Stable
 data class MainScreenState(val themeValues: String? = null,val userAuth:Boolean=false)
