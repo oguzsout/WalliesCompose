33c624f5e9609fd8eddb31ef7afac39dbe0ef709
oguzhandogdu <oguzd494@gmail.com>
2024-03-20 22:05:25 +0300

    Add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
     id("com.google.dagger.hilt.android") version "2.49" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
