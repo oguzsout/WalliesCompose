6886bedfd5296b95a7c987b5ddd138205cea430f
oguzhandogdu <oguzd494@gmail.com>
2024-06-02 15:26:58 +0300

    Edit bottom bar screens navigation structure with app state

--- app/src/main/java/com/oguzdogdu/walliescompose/features/favorites/FavoritesScreenNavigation.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/features/favorites/FavoritesScreenNavigation.kt
@@ -4,19 +4,19 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import kotlinx.serialization.Serializable
 
-const val FavoritesScreenNavigationRoute = "favorites_screen_route"
+@Serializable
+data object FavoritesScreenNavigationRoute
 
 fun NavController.navigateToFavoritesScreen(
     navOptions: NavOptions? = null,
 ) {
-    this.navigate(FavoritesScreenNavigationRoute, navOptions)
+    navigate(route = FavoritesScreenNavigationRoute, navOptions)
 }
 
 fun NavGraphBuilder.favoritesScreen(onFavoriteClick: (String?) -> Unit) {
-    composable(
-        FavoritesScreenNavigationRoute
-    ) {
+    composable<FavoritesScreenNavigationRoute> {
         FavoritesScreenRoute(onFavoriteClick = {
             onFavoriteClick.invoke(it)
         })
--- app/src/main/java/com/oguzdogdu/walliescompose/navigation/TopLevelDestination.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/navigation/TopLevelDestination.kt
@@ -5,30 +5,31 @@ import com.oguzdogdu.walliescompose.features.collections.CollectionScreenNavigat
 import com.oguzdogdu.walliescompose.features.favorites.FavoritesScreenNavigationRoute
 import com.oguzdogdu.walliescompose.features.home.HomeScreenNavigationRoute
 import com.oguzdogdu.walliescompose.features.settings.SettingsScreenNavigationRoute
+import kotlin.reflect.KClass
 
 enum class TopLevelDestination(
     val icon: Int,
     val iconTextId: Int,
-    val route: String,
+    val route: KClass<*>,
 ) {
     WALLPAPERS( 
         icon = R.drawable.wallpaper,
         iconTextId = R.string.wallpapers_title,
-        route = HomeScreenNavigationRoute
+        route = HomeScreenNavigationRoute::class
     ),
     COLLECTIONS(
         icon = R.drawable.collections,
         iconTextId = R.string.collections_title,
-        route = CollectionScreenNavigationRoute
+        route = CollectionScreenNavigationRoute::class
     ),
     FAVORITES(
         icon = R.drawable.favorite,
         iconTextId = R.string.favorites_title,
-        route = FavoritesScreenNavigationRoute
+        route = FavoritesScreenNavigationRoute::class
     ),
     SETTINGS(
         icon = R.drawable.settings,
         iconTextId = R.string.settings_title,
-        route = SettingsScreenNavigationRoute
+        route = SettingsScreenNavigationRoute::class
     ),
 }
