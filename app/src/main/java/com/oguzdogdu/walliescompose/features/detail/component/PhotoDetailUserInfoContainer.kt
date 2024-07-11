ed51383da11bc4e68848981c1d5993cbed5afde3
oguzhandogdu <oguzd494@gmail.com>
2024-07-11 15:50:25 +0300

    Add animation to detail screen lazy column

--- app/src/main/java/com/oguzdogdu/walliescompose/features/detail/component/PhotoDetailUserInfoContainer.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/features/detail/component/PhotoDetailUserInfoContainer.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -45,7 +46,7 @@ fun PhotoDetailUserInfoContainer(
             16.dp
         ), modifier = modifier
             .fillMaxWidth()
-            .wrapContentHeight()
+            .heightIn(min = 96.dp, max = 192.dp)
             .padding(vertical = 8.dp)
     ) {
         Box(
