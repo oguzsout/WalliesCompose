0e89ccccf22db08d155d3563decdf30416f5961c
MuhammetKDR <muhammetkudur@gmail.com>
2024-08-18 22:04:45 +0300

    remove language param

--- app/src/main/java/com/oguzdogdu/walliescompose/features/search/SearchScreen.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/features/search/SearchScreen.kt
@@ -108,8 +108,6 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
-import okhttp3.internal.immutableListOf
-import kotlin.reflect.jvm.internal.impl.descriptors.Visibilities.Local
 
 @Composable
 fun SearchScreenRoute(
@@ -127,7 +125,6 @@ fun SearchScreenRoute(
     val appLanguage by viewModel.appLanguage.collectAsStateWithLifecycle()
     val query by viewModel.query.collectAsStateWithLifecycle()
     val stateOfSearchScreen by viewModel.searchScreenState.collectAsStateWithLifecycle()
-    val currentLanguage = LocalContext.current.resources.configuration.locales[0].language
 
     LifecycleEventEffect(event = Lifecycle.Event.ON_CREATE) {
         viewModel.handleUIEvent(SearchEvent.GetAppLanguageValue)
@@ -138,7 +135,7 @@ fun SearchScreenRoute(
             viewModel.handleUIEvent(
                 SearchEvent.EnteredSearchQuery(
                     queryFromDetail.orEmpty(),
-                    currentLanguage
+                    null
                 )
             )
         }
