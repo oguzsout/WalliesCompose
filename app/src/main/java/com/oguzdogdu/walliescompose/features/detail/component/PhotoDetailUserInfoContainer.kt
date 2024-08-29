17d64292ca075ec880da8fec31ce3394d273552b
oguzhandogdu <oguzd494@gmail.com>
2024-08-29 17:36:05 +0300

    Add tooltip to detail attributes card

--- app/src/main/java/com/oguzdogdu/walliescompose/features/detail/component/PhotoDetailUserInfoContainer.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/features/detail/component/PhotoDetailUserInfoContainer.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowForward
