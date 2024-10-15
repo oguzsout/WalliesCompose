de1d8e9706e64a0240481426441d1764574dabeb
oguzdogdu <oguzd494@gmail.com>
2024-10-15 20:51:13 +0300

    Add folding animation to snackbar

--- app/src/main/java/com/oguzdogdu/walliescompose/features/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/features/favorites/FavoritesViewModel.kt
@@ -1,16 +1,18 @@
 package com.oguzdogdu.walliescompose.features.favorites
 
-import androidx.compose.material3.SnackbarDuration
 import androidx.lifecycle.viewModelScope
 import com.oguzdogdu.walliescompose.R
 import com.oguzdogdu.walliescompose.core.BaseViewModel
 import com.oguzdogdu.walliescompose.domain.repository.WallpaperRepository
+import com.oguzdogdu.walliescompose.features.appstate.Duration
+import com.oguzdogdu.walliescompose.features.appstate.MessageContent
 import com.oguzdogdu.walliescompose.features.appstate.MessageType
 import com.oguzdogdu.walliescompose.features.appstate.SnackbarModel
 import com.oguzdogdu.walliescompose.features.favorites.effect.FavoriteScreenEffect
 import com.oguzdogdu.walliescompose.features.favorites.event.FavoriteScreenEvent
 import com.oguzdogdu.walliescompose.features.favorites.state.FavoriteScreenState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,7 +24,7 @@ class FavoritesViewModel @Inject constructor(private val repository: WallpaperRe
         when (event) {
             is FavoriteScreenEvent.GetFavorites -> fetchImagesToFavorites()
             is FavoriteScreenEvent.FlipToImage -> setFlipCardState(event.flip)
-            is FavoriteScreenEvent.DeleteFromFavorites -> deleteWithIdFromToFavorites(event.favoriteId)
+            is FavoriteScreenEvent.DeleteFromFavorites -> deleteWithIdFromFavorites(event.favoriteId)
         }
     }
 
@@ -35,17 +37,28 @@ class FavoritesViewModel @Inject constructor(private val repository: WallpaperRe
                 sendEffect(FavoriteScreenEffect.ShowSnackbar(SnackbarModel(
                     type = MessageType.ERROR,
                     drawableRes = R.drawable.ic_cancel,
-                    message = error.message,
-                    duration = SnackbarDuration.Long
+                    message = MessageContent.PlainString(error.message.orEmpty()),
+                    duration = Duration.SHORT
                 )))
             },
             onComplete = {}
         )
     }
 
-    private fun deleteWithIdFromToFavorites(favoriteId:String) {
+    private fun deleteWithIdFromFavorites(favoriteId: String) {
         viewModelScope.launch {
-            repository.deleteSpecificIdFavorite(favoriteId)
+            async { repository.deleteSpecificIdFavorite(favoriteId) }.await()
+            repository.getFavorites().collect { favorites ->
+                val isDeleteSuccessful = favorites?.none { it.id == favoriteId } ?: true
+                if (isDeleteSuccessful) {
+                    sendEffect(FavoriteScreenEffect.ShowSnackbar(SnackbarModel(
+                        type = MessageType.SUCCESS,
+                        drawableRes = R.drawable.ic_completed,
+                        message = MessageContent.ResourceString(R.string.delete_favorites),
+                        duration = Duration.SHORT
+                    )))
+                }
+            }
         }
     }
 
