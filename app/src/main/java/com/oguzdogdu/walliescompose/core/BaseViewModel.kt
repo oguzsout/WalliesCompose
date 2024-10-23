393db01464ff5f7474a09fff20ea603fff5a8dba
oguzdogdu <oguzd494@gmail.com>
2024-10-23 21:37:47 +0300

    Fix nullability of animatable snackbar

--- app/src/main/java/com/oguzdogdu/walliescompose/core/BaseViewModel.kt
+++ app/src/main/java/com/oguzdogdu/walliescompose/core/BaseViewModel.kt
@@ -4,19 +4,16 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.onSuccess
-import kotlinx.coroutines.channels.trySendBlocking
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.receiveAsFlow
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
@@ -27,8 +24,8 @@ abstract class BaseViewModel<State : ViewState, Event : ViewEvent, Effect : View
     private val _state: MutableStateFlow<State> = MutableStateFlow(initialState)
     val state = _state.asStateFlow()
 
-    private val _effect: Channel<Effect> = Channel(Channel.BUFFERED)
-    val effect = _effect.receiveAsFlow()
+    private val _effect: MutableSharedFlow<Effect> = MutableSharedFlow(replay = 1, extraBufferCapacity = 4)
+    val effect = _effect.asSharedFlow()
 
     private val _eventChannel: Channel<Event> = Channel(Channel.BUFFERED)
     val eventChannel = _eventChannel.receiveAsFlow()
@@ -77,7 +74,7 @@ abstract class BaseViewModel<State : ViewState, Event : ViewEvent, Effect : View
 
     fun sendEffect(effect: Effect) {
         viewModelScope.launch {
-            _effect.trySend(effect)
+            _effect.emit(effect)
         }
     }
 
